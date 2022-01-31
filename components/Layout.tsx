cf46497261e9456eaf23d54b19057cf7b2c334a9
JC <39209141+jhc13@users.noreply.github.com>
2022-01-31 22:00:56 +0900

    Add logo and favicons

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,12 +1,20 @@
-import Link from 'next/link';
 import { ReactNode } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import logo from 'images/logo.svg';
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
-    <div className='h-screen dark:bg-zinc-800 bg-zinc-200 dark:text-zinc-100 text-zinc-900'>
-      <header className='sticky top-0 left-0 right-0 flex justify-center dark:bg-sky-900 bg-sky-600'>
-        <Link href='/'>
-          <a className='text-3xl leading-relaxed'>Mental Math Trainer</a>
+    <div className='h-screen bg-zinc-800 text-white'>
+      <header className='sticky top-0 left-0 right-0 flex justify-center py-2 bg-gray-800'>
+        <Link href='/' passHref>
+          <Image
+            src={logo}
+            alt='Mental Math Trainer logo'
+            width={32}
+            height={32}
+            className='cursor-pointer'
+          />
         </Link>
       </header>
       <div className='max-w-screen-md mx-auto mt-2 p-3'>{children}</div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,7 @@
 import type { NextPage } from 'next';
-import { useContext } from 'react';
-import { SettingsContext } from 'utils/settings';
 
 const Home: NextPage = () => {
-  const { settings, setSettings } = useContext(SettingsContext);
-
-  return (
-    <button
-      onClick={() => setSettings({ ...settings, darkMode: !settings.darkMode })}
-      className='rounded px-2 py-1 bg-lime-700 text-xl'
-    >
-      {`dark mode: ${settings.darkMode}`}
-    </button>
-  );
+  return <div />;
 };
 
 export default Home;
