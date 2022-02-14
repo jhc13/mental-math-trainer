9bcc2f3ff63a9e8c65bdb4a0b8a81c6475a2c4fe
JC <39209141+jhc13@users.noreply.github.com>
2022-02-15 00:11:36 +0900

    Add abort button

--- components/Solve.js
+++ components/Solve.js
@@ -1,4 +1,5 @@
 import { useContext } from 'react';
+import { XCircleIcon } from '@heroicons/react/solid';
 import useSolve from 'hooks/useSolve';
 import { SettingsContext } from 'utils/settings';
 import Timer from 'components/Timer';
@@ -20,7 +21,18 @@ function Solve({ onCorrectAnswer, onAbort }) {
 
   return (
     <div className='flex h-full flex-col items-center'>
-      {showTimerWhileSolving && <Timer startTime={startTime} />}
+      <div className='grid w-full grid-cols-3 place-items-center'>
+        <div className='col-start-2'>
+          {showTimerWhileSolving && <Timer startTime={startTime} />}
+        </div>
+        <button
+          className='col-start-3 flex items-center gap-1.5 justify-self-end rounded-md bg-red-900 px-2 py-1'
+          onClick={onAbort}
+        >
+          <XCircleIcon className='h-5 w-5' aria-hidden='true' />
+          <div className='text-lg'>Abort</div>
+        </button>
+      </div>
       <div className={'flex flex-auto items-center'}>
         <Problem
           operation={operation}
