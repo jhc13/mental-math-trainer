3e7acb60c20b2a85f3a8c5e49f0a7c42539bf66b
JC <39209141+jhc13@users.noreply.github.com>
2022-02-16 22:14:57 +0900

    Change header link back to Link

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import Sidebar from 'components/Sidebar';
 import logo from 'public/logo.svg';
 
@@ -7,19 +8,17 @@ export default function Layout({ children }) {
     <div className='fixed inset-0 flex flex-col overflow-auto bg-zinc-800 text-white'>
       <header className='grid h-12 grid-cols-[2.25rem_1fr_2.25rem] bg-gray-800 px-2'>
         <Sidebar />
-        {/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
-        <a
-          href='/'
-          className='flex select-none items-center gap-4 justify-self-center'
-        >
-          <Image
-            src={logo}
-            alt='Mental Math Trainer logo'
-            width={36}
-            height={36}
-          />
-          <div className='hidden text-xl sm:block'>Mental Math Trainer</div>
-        </a>
+        <Link href='/' passHref>
+          <a className='flex select-none items-center gap-4 justify-self-center'>
+            <Image
+              src={logo}
+              alt='Mental Math Trainer logo'
+              width={36}
+              height={36}
+            />
+            <div className='hidden text-xl sm:block'>Mental Math Trainer</div>
+          </a>
+        </Link>
       </header>
       <main className='mx-auto my-3 w-full max-w-screen-md flex-auto px-3'>
         {children}
