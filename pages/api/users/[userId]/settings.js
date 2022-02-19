ae5741df76d594cf6f6298ea4c4343bd0f90d15c
JC <39209141+jhc13@users.noreply.github.com>
2022-02-19 15:32:05 +0900

    Secure route

--- pages/api/users/[userId]/settings.js
+++ pages/api/users/[userId]/settings.js
@@ -1,7 +1,16 @@
+import { getSession } from 'next-auth/react';
 import prisma from 'prisma/prisma';
 
 export default async function handler(req, res) {
+  const session = await getSession({ req });
   const { userId } = req.query;
+  if (session === null) {
+    return res.status(401).end();
+  }
+  if (session.user.id !== userId) {
+    return res.status(403).end();
+  }
+
   if (req.method === 'GET') {
     const settings = await prisma.settings.findUnique({
       where: {
