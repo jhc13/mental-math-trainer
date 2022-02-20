29190319fcc8fed1b197bd84dda5c8eacfdc7517
JC <39209141+jhc13@users.noreply.github.com>
2022-02-20 21:36:02 +0900

    Set top section height

--- components/MenuSidebar.js
+++ components/MenuSidebar.js
@@ -68,24 +68,27 @@ export default function MenuSidebar() {
         <Disclosure.Panel className='absolute top-12 left-0 bottom-0 z-20 w-full select-none overflow-auto scroll-smooth bg-[#202022] px-4 pt-4 pb-32 text-lg sm:max-w-sm'>
           {({ close }) => (
             <div className='flex flex-col gap-4'>
-              {session ? (
-                <div className='flex flex-col gap-0.5'>
-                  <div className='text-center text-base'>Signed in as</div>
-                  <DisplayName userId={session.user.id} />
-                </div>
-              ) : (
-                <div className='flex flex-col gap-2'>
-                  <div className='text-center text-base'>Not signed in</div>
-                  <Link href='/auth/sign-in'>
-                    <a
-                      onClick={close}
-                      className='self-center rounded-md bg-cyan-800 px-3 py-1 active:brightness-[0.85]'
-                    >
-                      Sign in
-                    </a>
-                  </Link>
-                </div>
-              )}
+              <div className='h-16'>
+                {session ? (
+                  <div className='flex flex-col gap-0.5'>
+                    <div className='text-center text-base'>Signed in as</div>
+                    <DisplayName userId={session.user.id} />
+                  </div>
+                ) : (
+                  <div className='flex flex-col gap-1'>
+                    <div className='text-center text-base'>Not signed in</div>
+                    <Link href='/auth/sign-in'>
+                      <a
+                        onClick={close}
+                        className='self-center rounded-md bg-cyan-800 px-3 py-1 active:brightness-[0.85]'
+                      >
+                        Sign in
+                      </a>
+                    </Link>
+                  </div>
+                )}
+              </div>
+
               <Divider />
               <Link href='/'>
                 <a onClick={close} className='flex items-center gap-3'>
