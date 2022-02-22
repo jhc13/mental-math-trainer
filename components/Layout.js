8996cf92327f7134ad76613a7577af7a743c04dc
JC <39209141+jhc13@users.noreply.github.com>
2022-02-23 02:14:01 +0900

    Add comment

--- components/Layout.js
+++ components/Layout.js
@@ -8,29 +8,33 @@ export default function Layout({ children }) {
   const [topSidebar, setTopSidebar] = useState(null);
 
   return (
-    <div className='fixed inset-0 flex flex-col overflow-y-auto overflow-x-hidden bg-zinc-800 text-zinc-100'>
-      <header className='grid h-12 grid-cols-[2.25rem_1fr_2.25rem] bg-gray-800 px-2'>
-        <MenuSidebar
-          topSidebar={topSidebar}
-          onClick={() => {
-            setTopSidebar('MENU');
-          }}
-        />
-        <Link href='/' passHref>
-          <a className='flex select-none items-center gap-4 justify-self-center'>
-            <Logo className='h-9 w-9 fill-sky-500 stroke-sky-500' />
-            <div className='hidden text-xl sm:block'>Mental Math Trainer</div>
-          </a>
-        </Link>
-        <SettingsSidebar
-          onClick={() => {
-            setTopSidebar('SETTINGS');
-          }}
-        />
-      </header>
-      <main className='mx-auto my-3 w-full max-w-screen-md flex-auto px-3'>
-        {children}
-      </main>
-    </div>
+    <>
+      {/* Use overflow-x-hidden to prevent the scrollbar from showing up when
+          the right sidebar is opened or closed. */}
+      <div className='fixed inset-0 flex flex-col overflow-y-auto overflow-x-hidden bg-zinc-800 text-zinc-100'>
+        <header className='grid h-12 grid-cols-[2.25rem_1fr_2.25rem] bg-gray-800 px-2'>
+          <MenuSidebar
+            topSidebar={topSidebar}
+            onClick={() => {
+              setTopSidebar('MENU');
+            }}
+          />
+          <Link href='/' passHref>
+            <a className='flex select-none items-center gap-4 justify-self-center'>
+              <Logo className='h-9 w-9 fill-sky-500 stroke-sky-500' />
+              <div className='hidden text-xl sm:block'>Mental Math Trainer</div>
+            </a>
+          </Link>
+          <SettingsSidebar
+            onClick={() => {
+              setTopSidebar('SETTINGS');
+            }}
+          />
+        </header>
+        <main className='mx-auto my-3 w-full max-w-screen-md flex-auto px-3'>
+          {children}
+        </main>
+      </div>
+    </>
   );
 }
