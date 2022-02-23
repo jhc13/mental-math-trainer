9cab0d776227640e6ebf86f5b6ad1c9bb58bd642
JC <39209141+jhc13@users.noreply.github.com>
2022-02-24 01:20:59 +0900

    Highlight solves for selected best

--- components/SetResults.js
+++ components/SetResults.js
@@ -5,6 +5,7 @@ import { formatCentiseconds, OPERATORS, pluralize } from 'utils/format';
 
 export default function SetResults({ problems }) {
   const [bests, setBests] = useState(null);
+  const [selectedBest, setSelectedBest] = useState(null);
   const { data: session } = useSession();
 
   // Get the bests once on initial load.
@@ -34,18 +35,23 @@ export default function SetResults({ problems }) {
   const centisecondsPerProblem = Math.round(
     totalCentiseconds / problems.length
   );
-
-  const formatLabels = [<div key={-1}>Set mean:</div>];
-  const formatTimes = [
+  const bestLabels = [<div key={-1}>Set mean:</div>];
+  const bestTimes = [
     <div key={-1}>{formatCentiseconds(centisecondsPerProblem)}</div>
   ];
+  const handleBestClick = (best) => {
+    setSelectedBest(selectedBest === best ? null : best);
+  };
   if (bests) {
-    formatLabels.push(
+    bestLabels.push(
       ...bests.map((best, i) => {
         return (
           <div
             key={i}
-            className={best.isNewRecord ? 'font-semibold text-sky-500' : ''}
+            onClick={() => handleBestClick(best)}
+            className={`${
+              best.isNewRecord ? 'font-semibold text-sky-500' : ''
+            } cursor-pointer`}
           >{`Best ${
             best.problemCount === 1
               ? 'single'
@@ -56,12 +62,15 @@ export default function SetResults({ problems }) {
         );
       })
     );
-    formatTimes.push(
+    bestTimes.push(
       ...bests.map((best, i) => {
         return (
           <div
             key={i}
-            className={best.isNewRecord ? 'font-semibold text-sky-500' : ''}
+            onClick={() => handleBestClick(best)}
+            className={`${
+              best.isNewRecord ? 'font-semibold text-sky-500' : ''
+            } cursor-pointer`}
           >
             {formatCentiseconds(best.centiseconds)}
           </div>
@@ -72,7 +81,7 @@ export default function SetResults({ problems }) {
     const validFormats = recordFormats.filter(
       (format) => format.problemCount <= problems.length
     );
-    formatLabels.push(
+    bestLabels.push(
       ...validFormats.map((format, i) => {
         return (
           <div key={i}>
@@ -87,6 +96,11 @@ export default function SetResults({ problems }) {
         );
       })
     );
+    bestTimes.push(
+      ...validFormats.map((_, i) => {
+        return <div key={i}>...</div>;
+      })
+    );
   }
 
   return (
@@ -96,20 +110,47 @@ export default function SetResults({ problems }) {
           totalCentiseconds
         )}`}
       </h1>
-      <div className='grid grid-cols-2 text-lg tabular-nums'>
-        <div className='col-span-2 flex justify-center gap-3 sm:col-auto'>
-          <div className='text-left'>{formatLabels}</div>
-          <div className='text-right'>{formatTimes}</div>
+      <div className='flex flex-col gap-6 text-lg tabular-nums sm:grid sm:grid-cols-2'>
+        <div className='flex justify-center gap-3'>
+          <div className='flex flex-col gap-0.5 text-left'>{bestLabels}</div>
+          <div className='flex flex-col gap-0.5 text-right'>{bestTimes}</div>
         </div>
-        <div className='hidden max-h-[21rem] overflow-auto sm:block'>
-          <div className='mx-auto w-fit text-left'>
+        <div className='max-h-36 overflow-auto sm:max-h-[21rem]'>
+          <div className='grid grid-cols-[auto_auto] justify-center gap-y-0.5 gap-x-2'>
             {problems.map((problem, i) => {
               const { operation, operands, centiseconds } = problem;
               const operator = OPERATORS[operation];
               return (
-                <div key={i}>{`${operands[0]}${operator}${
-                  operands[1]
-                }: ${formatCentiseconds(centiseconds)}`}</div>
+                <>
+                  <div
+                    className={`${
+                      selectedBest &&
+                      i >= selectedBest.startIndex &&
+                      i < selectedBest.startIndex + selectedBest.problemCount
+                        ? selectedBest.excludedIndices.includes(i)
+                          ? 'text-yellow-400 line-through'
+                          : 'text-lime-400'
+                        : ''
+                    } px-1.5 text-right transition-colors`}
+                  >
+                    {i + 1}.
+                  </div>
+                  <div
+                    className={`${
+                      selectedBest &&
+                      i >= selectedBest.startIndex &&
+                      i < selectedBest.startIndex + selectedBest.problemCount
+                        ? selectedBest.excludedIndices.includes(i)
+                          ? 'text-yellow-400'
+                          : 'text-lime-400'
+                        : ''
+                    } px-1.5 transition-colors`}
+                  >
+                    {`${operands[0]}${operator}${
+                      operands[1]
+                    }: ${formatCentiseconds(centiseconds)}`}
+                  </div>
+                </>
               );
             })}
           </div>
