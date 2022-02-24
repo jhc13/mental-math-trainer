971a2c09a881215497af81934516b08f1a8595f1
JC <39209141+jhc13@users.noreply.github.com>
2022-02-25 02:05:15 +0900

    Show or hide set settings buttons based on device

--- components/Intermission.js
+++ components/Intermission.js
@@ -1,8 +1,16 @@
-import { useEffect } from 'react';
+import Link from 'next/link';
+import { useContext, useEffect } from 'react';
+import { useSession } from 'next-auth/react';
+import { SettingsContext } from 'utils/settings';
+import { OPERATORS, pluralize } from 'utils/format';
 import SetResults from 'components/SetResults';
 import SetSettings from 'components/SetSettings';
 
 export default function Intermission({ problems, onNewSet }) {
+  const { data: session } = useSession();
+  const { settings } = useContext(SettingsContext);
+  const { operation, operandLengths, setProblemCount } = settings;
+
   useEffect(() => {
     const handleKeyDown = ({ key }) => {
       if ([' ', 'Enter'].includes(key)) {
@@ -17,15 +25,36 @@ export default function Intermission({ problems, onNewSet }) {
 
   return (
     <>
-      <div className='mt-8 mb-5 flex flex-auto flex-col gap-4'>
+      <div className='mt-8 mb-5 flex flex-auto flex-col gap-8'>
         {problems && <SetResults problems={problems} />}
-        <SetSettings onNewSet={onNewSet} />
-        <button
-          onClick={onNewSet}
-          className='mx-auto block select-none rounded-lg bg-cyan-800 px-3.5 py-2 text-2xl font-medium active:brightness-[0.85]'
-        >
-          New Set
-        </button>
+        <div className={`${problems ? 'hidden sm:block' : ''} mx-auto`}>
+          <SetSettings onNewSet={onNewSet} />
+        </div>
+        <div className='flex flex-col items-center gap-1.5'>
+          <div
+            className={`${problems ? 'block sm:hidden' : 'hidden'} font-medium`}
+          >{`${pluralize('digit', operandLengths[0])} ${
+            OPERATORS[operation]
+          } ${pluralize('digit', operandLengths[1])}, ${pluralize(
+            'problem',
+            setProblemCount
+          )}`}</div>
+          <button
+            onClick={onNewSet}
+            className='select-none rounded-lg bg-cyan-800 px-3.5 py-2 text-2xl font-medium active:brightness-[0.85]'
+          >
+            New Set
+          </button>
+        </div>
+        {session === null && (
+          <p className='mx-auto w-fit px-8'>
+            <strong className='font-semibold'>You are not signed in.</strong>{' '}
+            <Link href='/auth/sign-in'>
+              <a className='text-blue-400 underline'>Sign in</a>
+            </Link>{' '}
+            to save your times and keep track of your progress.
+          </p>
+        )}
       </div>
     </>
   );
