329d942166b965751a89de9655e3bf8894063f88
JC <39209141+jhc13@users.noreply.github.com>
2022-02-27 23:22:11 +0900

    Show total problem count and time spent

--- pages/stats.js
+++ pages/stats.js
@@ -2,6 +2,7 @@ import { useRouter } from 'next/router';
 import Head from 'next/head';
 import useSWR from 'swr';
 import { getSession, useSession } from 'next-auth/react';
+import { formatCentiseconds } from '/utils/format';
 
 export async function getServerSideProps(context) {
   const session = await getSession(context);
@@ -41,8 +42,24 @@ export default function Stats() {
           content='View your stats, personal records and improvement over time.'
         />
       </Head>
-      <div className='mt-8'>
-        <h1 className='text-center text-xl font-semibold'>Stats</h1>
+      <div className='mt-8 flex flex-col gap-4'>
+        <h1 className='text-center text-2xl font-semibold'>Stats</h1>
+        <div className='grid justify-center gap-y-4 sm:grid-cols-2 sm:justify-items-center'>
+          <div>
+            <h2 className='text-lg font-medium'>
+              Total number of problems solved
+            </h2>
+            <div className='text-3xl font-medium'>
+              {data ? data._count : '...'}
+            </div>
+          </div>
+          <div>
+            <h2 className='text-lg font-medium'>Total time spent solving</h2>
+            <div className='text-3xl font-medium'>
+              {data ? formatCentiseconds(data._sum.centiseconds) : '...'}
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
