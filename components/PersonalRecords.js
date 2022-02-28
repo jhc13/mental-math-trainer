44164c6e3251cec1fdefdb2c70a9080661f42875
JC <39209141+jhc13@users.noreply.github.com>
2022-02-28 20:32:52 +0900

    Map records to fragments

--- components/PersonalRecords.js
+++ components/PersonalRecords.js
@@ -1,29 +1,7 @@
-import { formatCentiseconds } from '../utils/format';
+import { Fragment } from 'react';
+import { formatCentiseconds } from 'utils/format';
 
 export default function PersonalRecords({ records }) {
-  const recordElements = [];
-  if (records === undefined) {
-    recordElements.push('...');
-  } else if (records.length === 0) {
-    recordElements.push('None');
-  } else {
-    for (const [i, record] of records.entries()) {
-      recordElements.push(
-        <div key={2 * i} className='first-letter:uppercase'>
-          {record.problemCount === 1
-            ? 'single'
-            : `${record.calculationMethod.toLowerCase()} of ${
-                record.problemCount
-              }`}
-          :
-        </div>
-      );
-      recordElements.push(
-        <div key={2 * i + 1}>{formatCentiseconds(record.centiseconds)}</div>
-      );
-    }
-  }
-
   return (
     <div className='flex flex-col items-center gap-1'>
       <div className='text-lg font-medium'>Personal records</div>
@@ -34,7 +12,23 @@ export default function PersonalRecords({ records }) {
             : 'grid-cols-[auto_auto]'
         } grid w-fit  gap-x-6 gap-y-0.5 text-xl`}
       >
-        {recordElements}
+        {records === undefined
+          ? '...'
+          : records.length === 0
+          ? 'None'
+          : records.map((record, i) => (
+              <Fragment key={i}>
+                <div className='first-letter:uppercase'>
+                  {record.problemCount === 1
+                    ? 'single'
+                    : `${record.calculationMethod.toLowerCase()} of ${
+                        record.problemCount
+                      }`}
+                  :
+                </div>
+                {formatCentiseconds(record.centiseconds)}
+              </Fragment>
+            ))}
       </div>
     </div>
   );
